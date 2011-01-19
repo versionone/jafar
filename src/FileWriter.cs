1304c6c61db7085598481753ba729e22059f0107
Dan Lash <dan.lash@versionone.com>
2011-01-19 11:03:25 -0500

    file writer and system console classes

---
+++ src/FileWriter.cs
@@ -0,0 +1,12 @@
+﻿using System.IO;
+
+namespace Jafar
+{
+	public class FileWriter
+	{
+		public void write(string filename, string contents)
+		{
+			File.WriteAllText(filename, contents);
+		}
+	}
+}
---
+++ src/SystemConsole.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Jafar
+{
+	public class SystemConsole
+	{
+		public void log(string message)
+		{
+			Console.WriteLine(message);
+		}
+
+		public void print(string message)
+		{
+			Console.Write(message);
+		}
+	}
+}
