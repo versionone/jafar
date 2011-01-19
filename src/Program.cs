027b27f47fa9a91618bb52c24583155763407b3a
Dan Lash <dan.lash@versionone.com>
2011-01-19 11:04:00 -0500

    file_writer and system_console available in global scope - command line arguments run as javascript files

--- src/Program.cs
+++ src/Program.cs
@@ -1,14 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.IO;
+using Noesis.Javascript;
 
 namespace Jafar
 {
 	class Program
 	{
-		static void Main(string[] args)
+
+		public static int Main(string[] args)
 		{
+			var context = new JavascriptContext();
+
+			context.SetParameter("file_writer", new FileWriter());
+			context.SetParameter("system_console", new SystemConsole());
+
+			foreach (var file in args)
+			{
+				var fullPath = Path.GetFullPath(file);
+				context.Run(File.ReadAllText(fullPath) + ";null;");
+			}
+
+			return 0;
 		}
 	}
 }
