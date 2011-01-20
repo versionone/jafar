37e05a3932e9760c95006e55847983243ba8803a
Dan Lash <dan.lash@versionone.com>
2011-01-20 17:33:16 -0500

    expose a file writer to the context

---
+++ src/FileLoader.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Noesis.Javascript;
+
+namespace Jafar
+{
+	public class FileLoader
+	{
+		private readonly JavascriptContext _context;
+
+		public FileLoader(JavascriptContext context)
+		{
+			_context = context;
+		}
+
+		public string load(string filename)
+		{
+			filename = Path.Combine(Program.CurrentPath, filename);
+			if (!filename.EndsWith(".js")) filename = filename + ".js";
+			var fullPath = Path.GetFullPath(filename);
+			Program.CurrentPath = fullPath.Substring(0, fullPath.LastIndexOf(Path.DirectorySeparatorChar));
+			return File.ReadAllText(fullPath);
+		}
+	}
+}
--- src/Program.cs
+++ src/Program.cs
@@ -1,22 +1,30 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using Noesis.Javascript;
 
 namespace Jafar
 {
-	class Program
+	public class Program
 	{
+		public static string CurrentPath;
 
 		public static int Main(string[] args)
 		{
+			CurrentPath = Path.GetFullPath(".");
+
 			var context = new JavascriptContext();
 
-			context.SetParameter("file_writer", new FileWriter());
+			context.SetParameter("file_writer", new FileWriter()); //TODO: should probably namespace these in javascript
 			context.SetParameter("system_console", new SystemConsole());
-
+			context.SetParameter("file_loader", new FileLoader(context));
+			
 			foreach (var file in args)
 			{
 				var fullPath = Path.GetFullPath(file);
-				context.Run(File.ReadAllText(fullPath) + ";null;");
+				CurrentPath = fullPath.Substring(0, fullPath.LastIndexOf(Path.DirectorySeparatorChar));
+				var script = File.ReadAllText(fullPath);
+				if (!string.IsNullOrEmpty(script))
+					context.Run(script + ";null;");
 			}
 
 			return 0;
