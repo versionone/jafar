a4a5068fc9dc0b4df54738122623194260c811ee
Dan Lash <dan.lash@versionone.com>
2011-01-20 10:21:15 -0500

    overloads for console log

--- src/SystemConsole.cs
+++ src/SystemConsole.cs
@@ -4,7 +4,8 @@ namespace Jafar
 {
 	public class SystemConsole
 	{
-		public void log(string message)
+		
+		public void log(object message)
 		{
 			Console.WriteLine(message);
 		}
@@ -13,5 +14,61 @@ namespace Jafar
 		{
 			Console.Write(message);
 		}
+
+		#region params array doesn't work for javascript methods
+		public void log(string a, string b, string c, string d, string e, string f, string g, string h, string i, string j)
+		{
+			log(a, b, c, d, e, f, g, h, i);
+			Console.WriteLine(j);
+		}
+
+		public void log(string a, string b, string c, string d, string e, string f, string g, string h, string i)
+		{
+			log(a, b, c, d, e, f, g, h);
+			Console.WriteLine(i);
+		}
+
+		public void log(string a, string b, string c, string d, string e, string f, string g, string h)
+		{
+			log(a, b, c, d, e, f, g);
+			Console.WriteLine(h);
+		}
+
+		public void log(string a, string b, string c, string d, string e, string f, string g)
+		{
+			log(a, b, c, d, e, f);
+			Console.WriteLine(g);
+		}
+
+		public void log(string a, string b, string c, string d, string e, string f)
+		{
+			log(a, b, c, d, e);
+			Console.WriteLine(f);
+		}
+
+		public void log(string a, string b, string c, string d, string e)
+		{
+			log(a, b, c, d);
+			Console.WriteLine(e);
+		}
+
+		public void log(string a, string b, string c, string d)
+		{
+			log(a, b, c);
+			Console.WriteLine(d);
+		}
+
+		public void log(string a, string b, string c)
+		{
+			log(a, b);
+			Console.WriteLine(c);
+		}
+
+		public void log(string a, string b)
+		{
+			log(a);
+			Console.WriteLine(b);
+		}
+		#endregion
 	}
 }
